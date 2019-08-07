76a00d0aace4759f452fa29e654bc1b725b4b676
Jonghyun Park <parjong@gmail.com>
2019-08-07 23:01:36 +0900

    Manage URL via files (#26)
    Let us manage URL via files instead of CMake variables!
    
    Signed-off-by: Jonghyun Park <parjong@gmail.com>

--- infra/cmake/packages/BVLCCaffeSource-1.0/BVLCCaffeSourceConfig.cmake
+++ infra/cmake/packages/BVLCCaffeSource-1.0/BVLCCaffeSourceConfig.cmake
@@ -1,11 +1,10 @@
 function(_Import)
   alex_include(ExternalSource)
+  alex_include(AlexTools)
 
-  set(BVLCCAFFE_URL "https://github.com/BVLC/caffe/archive/1.0.tar.gz" CACHE STRING "BVLC Caffe 1.0 source archive URL")
+  alex_3rdparty_download(PACKAGE BVLCCaffe VERSION 1.0)
 
-  ExternalSource_Download(BVLCCAFFE DIRNAME BVLCCAFFE-1.0 URL "${BVLCCAFFE_URL}")
-
-  set(BVLCCaffeSource_DIR "${BVLCCAFFE_SOURCE_DIR}" PARENT_SCOPE)
+  set(BVLCCaffeSource_DIR "${BVLCCaffe_DIR}" PARENT_SCOPE)
   set(BVLCCaffeSource_FOUND TRUE PARENT_SCOPE)
 endfunction(_Import)
 
--- infra/cmake/packages/GoogleTestSource-1.8.0/GoogleTestSourceConfig.cmake
+++ infra/cmake/packages/GoogleTestSource-1.8.0/GoogleTestSourceConfig.cmake
@@ -1,14 +1,10 @@
 function(_GoogleTestSource_Download)
   alex_include(ExternalSource)
+  alex_include(AlexTools)
 
-  # TODO Allow users to control whether to download source code or not
-  # TODO Allow users to specify URL for specific version with the same CMake configuration
-  set(GOOGLETEST_URL "https://github.com/google/googletest/archive/release-1.8.0.tar.gz" CACHE STRING "Google Test 1.8.0 source URL")
+  alex_3rdparty_download(PACKAGE GoogleTest VERSION 1.8.0)
 
-  ExternalSource_Download(GOOGLETEST DIRNAME GOOGLETEST-1.8.0 URL "${GOOGLETEST_URL}")
-
-  # Set FALSE on download failure
-  set(GoogleTestSource_DIR "${GOOGLETEST_SOURCE_DIR}" PARENT_SCOPE)
+  set(GoogleTestSource_DIR "${GoogleTest_DIR}" PARENT_SCOPE)
   set(GoogleTestSource_FOUND TRUE PARENT_SCOPE)
 endfunction(_GoogleTestSource_Download)
 
