962a808c3297cb0fb7366f4a1d18ce7e929dcedf
Jonghyun Park <parjong@gmail.com>
2021-08-01 21:45:54 +0900

    Introduce 'python3' domain (#65)
    This commit extends applet framework with 'python3' domain.
    
    Signed-off-by: Jonghyun Park <parjong@gmail.com>

---
+++ infra/applet/domain/python3/README.md
@@ -0,0 +1,7 @@
+## Manifest Format
+
+### 1.0
+
+```
+SCHEMA="python3:r1.0"
+```
---
+++ infra/applet/domain/python3/samples/r1.0/alex.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+print('Hello, World from Python 3 Applet')
+print('numpy.__version__ = {}'.format(np.__version__))
