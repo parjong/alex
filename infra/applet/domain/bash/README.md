c7cdb8953d95510277064d23aee23867534b25c1
Jonghyun Park <parjong@gmail.com>
2021-08-08 14:49:18 +0900

    Introduce 'bash' domain
    This commit introduces a new applet domain called 'bash'!
    
    Signed-off-by: Jonghyun Park <parjong@gmail.com>

---
+++ infra/applet/domain/bash/README.md
@@ -0,0 +1,7 @@
+## Manifest Format
+
+### 1.0
+
+```
+SCHEMA="bash:r1.0"
+```
---
+++ infra/applet/domain/bash/samples/r1.0/alex.sh
@@ -0,0 +1,4 @@
+# ALEX_APPLET_CODE_DIR points to the directory that includes alex.manifest.
+SELF_DIR="$ALEX_APPLET_CODE_DIR"
+
+cat "$SELF_DIR/MSG"
